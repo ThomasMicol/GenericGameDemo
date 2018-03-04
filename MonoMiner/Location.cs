690bfe53aa44fd9a895eb36504a34307f2a3411e
Thomas Micol <thomasmicol@gmail.com>
2018-03-04 17:19:24 +1300

    Added get vector for the drawing of sprites to the screen this is likely to be removed anyway

---
+++ MonoMiner/Location.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoMiner
+{
+    class Location
+    {
+        protected int x;
+        protected int y;
+        protected Game myContext;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Location(int inX, int inY, Game aContext)
+        {
+            myContext = aContext;
+            X = inX;
+            Y = inY;
+        }
+
+        public Vector2 GetVector2()
+        {
+            Vector2 vect = new Vector2(X, Y);
+            return vect;
+        }
+    }
+}
