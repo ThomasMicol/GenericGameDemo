1e18363062e7f42f49c5ab6ead8b2a1c0cdd6324
Thomas Micol <thomasmicol@gmail.com>
2018-03-05 15:35:57 +1300

    Added the Directions Enum to more accurately define the way in which the player moves around the game screen.

---
+++ MonoMiner/Directions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoMiner
+{
+    public enum Directions
+    {
+        East = 1,
+        South = 1,
+        West = -1,
+        North = -1
+    }
+}
